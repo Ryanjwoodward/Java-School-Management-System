8fc8e8b0688e3628e6a93409a1f62466b95216e8
Ryan Woodward <ryanjwoodward@outlook.com>
2022-06-10 03:42:18 -0700

    Project Start
    Opened IDE in repo folder and created Project folder, project packages, and classes

---
+++ School Management System/src/com/ryan/schoolmanagementsystem/Student.java
@@ -0,0 +1,11 @@
+package com.ryan.schoolmanagementsystem;
+
+/**
+ * This class is responsible for keeping track of 
+ * student first name, last name, id, grades, and fees paid
+ * @author _ryan
+ *
+ */
+public class Student {
+
+}
