2ed57cc6b5ce091524b1e40bec21957e0e411777
Ryan Woodward <ryanjwoodward@outlook.com>
2022-06-10 16:26:00 -0700

    wrote the basic setup for student class
    coded the basic code for Student class including fields, constructor, getters, and setters.

--- School Management System/src/com/ryan/schoolmanagementsystem/Student.java
+++ School Management System/src/com/ryan/schoolmanagementsystem/Student.java
@@ -8,4 +8,54 @@ package com.ryan.schoolmanagementsystem;
  */
 public class Student {
 
-}
+	private int studentId;
+	private String name;
+	private int grade;
+	private int feesPaid;
+	private int feesTotal;
+	
+	
+	//************************
+	//Constructors
+	//************************
+	
+	/**
+	 * Non-default constructor to create a Student Object
+	 * fees for each student is $30000/yr
+	 * fees paid initially is 0
+	 * @param id		the students id
+	 * @param name		the students name
+	 * @param grade		the students grade
+	 */
+	public Student(int id, String name, int grade) {
+		
+		feesPaid = 0;
+		this.studentId = id;
+		this.name = name;
+		this.grade = grade;
+	}
+	
+	//************************
+	//Getters and Setters
+	//No setters for students name or Id
+	
+	/**
+	 * Setter to update the grade field of a student
+	 * @param grade	 the new grade of the student
+	 */
+	public void setGrade(int grade) {
+		
+		this.grade = grade;
+	}
+	
+	/**
+	 * Update the total of fees paid by the student to the school
+	 * Adds the passed parameter value to the existing fees paid total
+	 * @param fees 		the value to be added to total fees paid by student
+	 */
+	public void updateFeesPaid(int fees) {
+		
+		this.feesPaid += fees;
+	}
+	
+}//Student Class
